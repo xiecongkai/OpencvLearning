6b916c979937fd6550f226287dcd58667549c2ca
xiecongkai <xck18438622355@163.com>
2023-02-21 11:44:21 +0800

    Create main.cpp

---
+++ demo01/demo001/main.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <qdebug.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <quickopencv.h>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    Mat src =imread("/home/diana/opencv_qt/demo01/Debug/1234/test.png");
+    if(src.empty()){
+        qDebug("could not load image...");
+        return -1;
+    }
+    namedWindow("src",WINDOW_AUTOSIZE);
+    imshow("输入窗口",src);
+
+    QuickDemo qd;
+    qd.colorSpace_Demo(src);
+
+    waitKey(0);
+
+    return a.exec();
+}
