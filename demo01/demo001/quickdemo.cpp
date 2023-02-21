cea9c126206c84fc34b6c7e082c2e264555b59d9
xiecongkai <xck18438622355@163.com>
2023-02-21 14:26:40 +0800

    Update quickdemo.cpp

--- demo01/demo001/quickdemo.cpp
+++ demo01/demo001/quickdemo.cpp
@@ -9,3 +9,15 @@ void QuickDemo::colorSpace_Demo(Mat &image){
     imwrite("/home/diana/opencv_qt/demo01/Debug/1234/hsv.png",hsv);
     imwrite("/home/diana/opencv_qt/demo01/Debug/1234/gray.png",gray);
 }
+
+
+void QuickDemo::mat_creation_demo(Mat &image){
+    Mat m1,m2;
+    m1 = image.clone();
+    image.copyTo(m2);
+
+    //创建空白图像
+    Mat m3 = Mat::zeros(Size(8,8),CV_8UC1);
+    std::cout<<"width:"<<m3.cols<<"height:"<<m3.rows<<"channels:"<<m3.channels()<<std::endl;
+    std::cout<<m3<<std::endl;
+}
